da216a58f6060bd62ee9cf2c2a16d187a3d48afc
Criztian Haunsen <cristian@mundodescuento.com>
2016-09-07 13:52:22 +0000

    helper to render custom element

--- app.lua
+++ app.lua
@@ -2,7 +2,22 @@ local lapis = require("lapis")
 local app = lapis.Application()
 app:enable("etlua")
 app.layout = require "views.themes.AdminLTE2.default"
-
+-- Load some components
+app:before_filter(function(self)
+  -- This load a custom element or the default element
+  -- views.element.footer
+  -- or
+  -- views.themes.AdminLTE2.element.footer
+  self.element = function(self, fn, element)
+    status, result = pcall(function()
+      return fn("views." .. element)
+    end)
+    if not status then
+      return fn("views.themes.AdminLTE2." .. element)
+    end
+    return result
+  end
+end)
 app:get("/", function()
   return "Welcome to Lapis " .. require("lapis.version")
 end)
